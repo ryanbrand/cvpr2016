d3300b16f59adfeeb7c3f227a8f62d18c4337209
Ryan Brand <rbrand1124@gmail.com>
2018-04-03 20:20:02 +0000

    Modified training scripts

--- scripts/train_flowers_hybrid.sh
+++ scripts/train_flowers_hybrid.sh
@@ -1,6 +1,6 @@
 
 th train_sje_hybrid.lua \
-  -data_dir data/flowers \
+  -data_dir /home/rmb2208/cvpr_data/flowers \
   -image_dir images \
   -ids_file trainvalids.txt \
   -learning_rate 0.0007 \
@@ -8,6 +8,6 @@ th train_sje_hybrid.lua \
   -max_epochs 200 \
   -savefile sje_flowers_c10_hybrid \
   -num_caption 10 \
-  -gpuid 2 \
+  -gpuid 0 \
   -print_every 10
 
